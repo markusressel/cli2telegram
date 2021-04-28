5822d98e2620619eb698a23dcee17f5ffa8807fe
Markus Ressel <markus9100@gmail.com>
2021-04-29 00:34:55 +0200

    v1.1.0 (#9)
    * v1.1.0
    * added python 3.9 to tests

--- setup.py
+++ setup.py
@@ -18,35 +18,13 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 #
-import os
-import subprocess
 
 from setuptools import setup, find_packages
 
-VERSION_NUMBER = "1.0.0"
+VERSION_NUMBER = "1.1.0"
 
-GIT_BRANCH = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-GIT_BRANCH = GIT_BRANCH.decode()  # convert to standard string
-GIT_BRANCH = GIT_BRANCH.rstrip()  # remove unnecessary whitespace
-
-if GIT_BRANCH == "master":
-    DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
-    VERSION_NAME = VERSION_NUMBER
-elif GIT_BRANCH == "beta":
-    DEVELOPMENT_STATUS = "Development Status :: 4 - Beta"
-    VERSION_NAME = "%s-beta" % VERSION_NUMBER
-elif GIT_BRANCH == "dev":
-    DEVELOPMENT_STATUS = "Development Status :: 3 - Alpha"
-    VERSION_NAME = "%s-dev" % VERSION_NUMBER
-elif (os.environ.get("TRAVIS_BRANCH", None) == os.environ.get("TRAVIS_TAG", None) == "v{}".format(VERSION_NUMBER)) or \
-        "GITHUB_RELEASE" in os.environ:
-    # travis tagged release branch
-    DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
-    VERSION_NAME = VERSION_NUMBER
-else:
-    print("Unknown git branch, using pre-alpha as default")
-    DEVELOPMENT_STATUS = "Development Status :: 2 - Pre-Alpha"
-    VERSION_NAME = "%s-%s" % (VERSION_NUMBER, GIT_BRANCH)
+DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
+VERSION_NAME = VERSION_NUMBER
 
 
 def readme_type() -> str:
@@ -99,7 +77,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=install_requirements(),
     tests_require=test_requirements(),
